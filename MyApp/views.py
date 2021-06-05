4c241e756a6a67f8365553c898f540dcfdd8ff11
Neeraj Joshi <neerajjoshi2308@gmail.com>
2021-06-05 18:21:09 +0530

    Added templates and db handlings

--- MyApp/views.py
+++ MyApp/views.py
@@ -10,7 +10,6 @@ def home(request):
     return render(request, 'index.html')
 
 def contact_us(request):
-    messages.success(request, '')
     if request.method == "POST":
         name = request.POST.get('name')
         email = request.POST.get('email')
@@ -21,5 +20,4 @@ def contact_us(request):
         contact = Contact(name=name, email=email, phone=phone, desc=desc, date=date)
         contact.save() 
         messages.success(request, 'Details submitted successfully.')
-
     return render(request, 'contact-us.html')
