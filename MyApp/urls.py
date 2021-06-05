196fe0bfd6ca642806bdbfe6d4b67b7bcdffc5ea
Neeraj Joshi <neerajjoshi2308@gmail.com>
2021-06-05 14:17:56 +0530

    added template

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('contact-us/', views.contact_us, name='contact_us')
 ]
--- MyApp/views.py
+++ MyApp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def contact_us(request):
+    return render(request, 'contact-us.html')
