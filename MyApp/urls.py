eb839cef418ced8f22f7b87a3ee864ee14bf3510
Neeraj Joshi <neerajjoshi2308@gmail.com>
2021-06-05 15:06:55 +0530

    Added template inheritance

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact-us/', views.contact_us, name='contact_us')
+    path('contact-us', views.contact_us, name='contact_us')
 ]
